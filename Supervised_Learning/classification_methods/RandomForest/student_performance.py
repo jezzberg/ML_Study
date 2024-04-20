80f82d300b7196edc75122dd59bb82e71e7da61e
Madalina <stratulatmadalina9@gmail.com>
2024-04-21 00:30:03 +0300

    forgot to add G1 & G2 to features

--- Supervised_Learning/classification_methods/RandomForest/student_performance.py
+++ Supervised_Learning/classification_methods/RandomForest/student_performance.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from ucimlrepo import fetch_ucirepo 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.metrics import  accuracy_score, classification_report 
 import numpy as np
@@ -16,12 +16,24 @@ with open('Data_visualization.txt', 'w') as file:
     file.write(f"\n\n1: \n{student_performance}")
   
 # get data as DataFrame  + write all data to data_visualization.txt in order to analyze & understand the dataset
+
 x = pd.DataFrame(student_performance.data.features)
 with open('data_visualization.txt', 'a') as file:
     file.write(f"\n\n1 - Features: \n{x}")
 y = pd.DataFrame(student_performance.data.targets) 
 with open('data_visualization.txt', 'a') as file:
     file.write(f"\n\n2 - Targets: \n{y}")  
+
+# Drop the first two columns from y_train and assign them to a separate DataFrame
+additional_features = y.iloc[:, :2]
+
+# Drop the first two columns from y_train
+y = y.iloc[:, 2:]
+
+# Concatenate additional_features with x_train
+x = pd.concat([x, additional_features], axis=1)
+
+
 
 # metadata 
 with open('data_visualization.txt', 'a') as file:
@@ -35,23 +47,22 @@ with open('data_visualization.txt', 'a') as file:
 # split data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
 
-
-
-
 # Step 2: Encode and split the data into training and testing subsets
 # Encode categorical variables
 print(f"cols types: \n{x_train.dtypes}")
-# encoder = label_encoder.OrdinalEncoder(cols=['school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic'])
+encoder = label_encoder.OrdinalEncoder(cols=['school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic'])
 
 # Step 6: build the model based on the most 5 important features
-columns_to_drop = ['Fedu', 'Mjob', 'famrel', 'Walc', 'reason', 'Medu', 'studytime',
-                   'Fjob', 'traveltime', 'Dalc', 'guardian', 'failures', 'famsup',
-                   'activities', 'romantic', 'sex', 'famsize', 'school', 'address',
-                   'internet', 'nursery', 'Pstatus', 'schoolsup', 'higher', 'paid']
+# columns_to_drop = ['Fedu', 'Mjob', 'famrel', 'Walc', 'reason', 'Medu', 'studytime',
+#                    'Fjob', 'traveltime', 'Dalc', 'guardian', 'failures', 'famsup',
+#                    'activities', 'romantic', 'sex', 'famsize', 'school', 'address',
+#                    'internet', 'nursery', 'Pstatus', 'schoolsup', 'higher', 'paid']
 
-x_train = x_train.drop(columns_to_drop, axis=1)
-x_test = x_test.drop(columns_to_drop, axis=1)
+# x_train = x_train.drop(columns_to_drop, axis=1)
+# x_test = x_test.drop(columns_to_drop, axis=1)
 
+x_train = encoder.fit_transform(x_train)
+x_test = encoder.fit_transform(x_test)
 
 print(f"x_train encoded: \n{x_train.head()}")
 
@@ -79,3 +90,17 @@ cm = confusion_matrix(y_test, y_pred)
 print('Confusion matrix\n\n', cm)
 
 print(classification_report(y_test, y_pred))
+
+print("Shape of x_train:", x_train.shape)
+print("Shape of y_train:", y_train.shape)
+
+
+# Step 7: perform cross-validation
+cv_scores = cross_val_score(rf_classifier, x_train, np.ravel(y_train), cv=5, scoring='accuracy')
+
+# Print the cross-validation scores
+print("Cross-Validation Scores:", cv_scores)
+
+# Print the mean and standard deviation of the cross-validation scores
+print("Mean Accuracy:", np.mean(cv_scores))
+print("Standard Deviation of Accuracy:", np.std(cv_scores))
