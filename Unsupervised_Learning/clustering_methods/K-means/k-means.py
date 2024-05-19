1246ed5d3acb98ce68d71564e5dc1e3eb5443223
Madalina <stratulatmadalina9@gmail.com>
2024-05-19 16:03:43 +0300

    k-means update

--- Unsupervised_Learning/clustering_methods/K-means/k-means.py
+++ Unsupervised_Learning/clustering_methods/K-means/k-means.py
@@ -6,68 +6,69 @@ import math
 colorMap = plt.get_cmap('Accent')
 
 # read points from file
-inFile = open("points.txt", 'r')
-number_of_observed_points = int(inFile.readline()) # will be 100 as the txt file has first line = 100 and is equal to total no# of entries
-points = np.zeros([number_of_observed_points, 2])
-for point_index in range(number_of_observed_points):
+inFile = open('points.txt', 'r')
+N = int(inFile.readline())
+points = np.zeros([N, 2])
+for i in range(N):
     pt = inFile.readline()
     pt = pt.split()
-    points[point_index, 0] = float(pt[0]) # x
-    points[point_index, 1] = float(pt[1]) # y
+    points[i, 0] = float(pt[0]) #x
+    points[i, 1] = float(pt[1]) #y
 
-k_clusters = 3
+K = 3
 
-# generate random colors, one for each cluster based on k_clusters values
-clusterColor = colorMap(np.array(range(k_clusters))/k_clusters)
+# generate random colors, one for each cluster
+clusterColor = colorMap(np.array(range(K))/K)
 
 # initialize distances from each point to corresponding centroid
-dist = np.zeros(number_of_observed_points)
+dist = np.zeros(N)
 
 # ids of clusters for each point: 
 # clusterID[i] = -1 means point i doesn't belong to any cluster
-# clusterID[i] = j means point i belongs to cluster j, j=[0..k_clusters-1]
-clusterID = np.full(number_of_observed_points, -1)
+# clusterID[i] = j means point i belongs to cluster j, j=[0..K-1]
+clusterID = np.full(N, -1)
 
-# randomly assign points to clusters
-clusterID = np.random.randint(0, k_clusters, number_of_observed_points)
+#randomly assign points to clusters
+clusterID = np.random.randint(0, K, N)
 
-centroids = np.zeros([k_clusters, 2]) # positions of centroids
+centroids = np.zeros([K, 2]) # positions of centroids
 
-# k_clustersmeans iterations
+#kmeans iterations
 
 nrMaxIterations = 10
 
 for iter in range(nrMaxIterations):
+    pass
 
     ### compute new centroids
-    avg = np.zeros([k_clusters, 2])
-    clusters_vectors = np.zeros([k_clusters, 2])
-    stop = False
+    avg = np.zeros([K, 2])
+    nrclusters = np.zeros([K, 2])
+    stopSeq = False
     
     # centroids[j] = average of points i with clusterID[i] == j      
-    for i in range(number_of_observed_points):
-        avg[clusterID[i]] += points[i] # sum up the coordinates of random ID vector given to point[i]
-        clusters_vectors[clusterID[i]] += 1
+    for i in range(N):
+        avg[clusterID[i]] += points[i]
+        nrclusters[clusterID[i]] += 1
         
-    for i in range(k_clusters):
-        avg[i] = avg[i]/clusters_vectors[i]
+    for i in range(K):
+        avg[i] = avg[i]/nrclusters[i]
         
         aux = abs(avg[i] - centroids[i])
-        # break_clusters when centroid positions don't change significantly from previous values
+        # break when centroid positions don't change significantly from previous values
         if aux[0]  > 0.1 and aux[1] > 0.1:
-            stop = True
+            stopSeq = True
            
         
         centroids[i] = avg[i]
         
-    if not stop:
+    if not stopSeq:
         print("stop")
-        break    
+        break      
     
     ### assign points to clusters
-    for i in range(number_of_observed_points):
+    for i in range(N):
         bestDistance = sys.maxsize
-        for j in range(k_clusters):
+        for j in range(K):
             currentDistance = math.dist(points[i],centroids[j])
             
             if currentDistance < bestDistance:
@@ -75,15 +76,15 @@ for iter in range(nrMaxIterations):
                 clusterID[i] = j
          
      
-    # for any point i, clusterID[i] = j, where j is the index of the centroid closest to i,  i = [0..N-1] , j =[0..k_clusters-1]
+    # for any point i, clusterID[i] = j, where j is the index of the centroid closest to i,  i = [0..N-1] , j =[0..K-1]
 
 #end for
 
 # plot points, centroids
 pointColors = np.array(clusterColor[clusterID])
-plt.scatter(points[:,0], points[:,1], color = pointColors, mark_clusterser = 'o', s = 10)
-centroidColors = np.array(clusterColor[range(k_clusters)])
-plt.scatter(centroids[:, 0], centroids[:, 1], color = centroidColors, mark_clusterser = 'x', s = 100)
+plt.scatter(points[:,0], points[:,1], color = pointColors, marker = 'o', s = 10)
+centroidColors = np.array(clusterColor[range(K)])
+plt.scatter(centroids[:, 0], centroids[:, 1], color = centroidColors, marker = 'x', s = 100)
 plt.show()    
 
 
